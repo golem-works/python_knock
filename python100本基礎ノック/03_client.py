6487da41b4e81e5debd7f74b4b6f306c45513139
kooooichi <139732089+golem-works@users.noreply.github.com>
2025-07-29 16:37:26 +0900

    Update 03_client.py

--- python100本基礎ノック/03_client.py
+++ python100本基礎ノック/03_client.py
@@ -1,19 +1,25 @@
-import os
+### 03_client.py
+### 悪用厳禁です。socket通信の側のクライアント側のコードです。
+
 import subprocess
 import socket
 
-IP = "127.0.0.1"
-PORT = 9000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((IP,PORT))
-    
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
+    c.connect(("127.0.0.1", 9000))
+    print("接続完了")
+
     while True:
-            cmd = input("shell >>").strip()
-            if cmd in ('exit' 'quit'):
-                s.sendall(cmd.encode())
+        try:
+            cmd = input("SHELL>>:").strip()
+            if not cmd:
+                continue
+            if cmd.lower() == "exit":
                 break
-            s.sendall(cmd.encode())
-            result=s.recv(4096).decode()
-            print(result)
-            
+            c.sendall(cmd.encode())
+
+            output = c.recv(4096).decode()
+            print(output)
+        except Exception as e:
+            print(f"エラー: {str(e)}")
+            break
